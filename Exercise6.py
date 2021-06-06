5fddb797a53bfa497cd28f92d5f34fb150587dec
= <=>
2021-06-06 13:59:42 +0300

    Exercise 6

---
+++ Exercise6.py
@@ -0,0 +1,22 @@
+'''
+6. Write a Python program that matches a string that has an a followed by two to three 'b'.
+
+'''
+
+import re
+def text_match(text):
+        patterns = 'ab{2,3}'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+
+print(text_match("ab"))
+print(text_match("abb"))
+print(text_match("abbb"))
+print(text_match("abbbb"))
+print(text_match("bbba"))
+print(text_match("abbabb"))
+print(text_match("aabbbbbc"))
+
