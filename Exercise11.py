624e5ef6fffd5dcc16f529d17f9700ab708a552b
= <=>
2021-06-06 14:49:50 +0300

    Exercise 11

---
+++ Exercise11.py
@@ -0,0 +1,16 @@
+'''
+11. Write a Python program that matches a word at the end of string, with optional punctuation.
+
+'''
+
+import re
+def text_match(text):
+        patterns = '\w+\S*$'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+print(text_match("The quick brown fox jumps over the lazy dog."))
+print(text_match("The quick brown fox jumps over the lazy dog. "))
+print(text_match("The quick brown fox jumps over the lazy dog "))
